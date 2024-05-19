dcd8a0dd139ad67fc63758cde1b5eeed99e4469d
Samir <sobiroff23@gmail.com>
2024-05-19 16:50:17 +0500

    fixed

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-document.querySelector('.rocket').addEventListener('click', function() {
-    const rocketPhoto = document.querySelector('.rocket_photo');
-    const rocketText = document.querySelector('.rocket_text');
+        document.querySelector('.rocket').addEventListener('click', function() {
+            const rocketPhoto = document.querySelector('.rocket_photo');
+            const textBlock = document.querySelector('.text');
+            
+            if (textBlock.classList.contains('visible')) {
+                rocketPhoto.style.width = '400px';
+                textBlock.classList.remove('visible');
+            } else {
+                rocketPhoto.style.width = '500px';
+                textBlock.classList.add('visible');
+            }
+        });
     
-    if (rocketText.classList.contains('visible')) {
-        rocketPhoto.style.width = '400px';
-        rocketText.classList.remove('visible');
-    } else {
-        rocketPhoto.style.width = '500px';
-        rocketText.classList.add('visible');
-    }
-});
