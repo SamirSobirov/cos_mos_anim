5cc124111bf7a00d5bd848968128a96a7ac2b696
Samir <sobiroff23@gmail.com>
2024-05-19 18:54:09 +0500

    fixed

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-        document.querySelector('.rocket').addEventListener('click', function() {
-            const rocketPhoto = document.querySelector('.rocket_photo');
-            const textBlock = document.querySelector('.text');
-            
-            if (textBlock.classList.contains('visible')) {
-                rocketPhoto.style.width = '400px';
-                textBlock.classList.remove('visible');
-            } else {
-                rocketPhoto.style.width = '500px';
-                textBlock.classList.add('visible');
-            }
-        });
-    
+
+document.querySelector('.rocket_photo').addEventListener('click', function() {
+    const rocketPhoto = document.querySelector('.rocket_photo');
+    const textBlock = document.querySelector('.text');
+    const rocket = document.querySelector('.rocket');
+
+    if (textBlock.classList.contains('visible')) {
+        rocketPhoto.style.width = '400px';
+        rocket.style.transform = 'translateX(0)';
+        textBlock.classList.remove('visible');
+    } else {
+        rocketPhoto.style.width = '600px';
+        rocket.style.transform = 'translateX(800)'; // Перемещение вправо
+        textBlock.classList.add('visible');
+    }
+});
